48f0f929b60d8a1fbc642a31f61e5c590944d2af
asdf2164 <112858163+asdf2164@users.noreply.github.com>
2024-05-01 11:47:32 +0800

    Add files via upload

---
+++ week11/week11-3.cpp
@@ -0,0 +1,16 @@
+/// week11-3.cpp 最大公因數 輾轉相除法
+#include <stdio.h>
+int gcd(int a,int b ){
+    if(a==0) return b;
+    if(b==0) return a;
+    return gcd(b,a%b);
+
+}
+int main(){
+    int a,b;
+    printf("請輸入2個數字:");
+    scanf("%d %d",&a,&b);
+    int ans = gcd(a,b);
+    printf("答案是:%d",ans);
+
+}
